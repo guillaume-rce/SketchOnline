b954e0a7af1ec5c04487f44fb533f3c50ef47bb2
Guillaume ROCHE <groche1217@protonmail.com>
2024-01-30 13:32:26 +0100

    Patch php

--- Frontend/Scripts/redirectToMy.js
+++ Frontend/Scripts/redirectToMy.js
@@ -2,7 +2,7 @@ function onRedirectToMy() {
     var data = localStorage.getItem('userData');    
     var id = JSON.parse(data).userId;
 
-    Api.request("/SketchOnline/Backend/role.php", "POST", {userId: id})
+    Api.request("/SketchOnline/Backend/getRole.php", "POST", {userId: id})
         .then(response => {
             console.log(response.status);
             response.status === 'success' ? OnRoleSuccess(response) : OnRoleError(response);
