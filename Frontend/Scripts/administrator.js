07134473dba0b11eaaca29ae05ba4331065f6c75
Guillaume ROCHE <groche1217@protonmail.com>
2024-01-30 20:34:11 +0100

    Concours + suppr des noms de variable peu orthodoxe

--- Frontend/Scripts/administrator.js
+++ Frontend/Scripts/administrator.js
@@ -1,9 +1,51 @@
-import Api from '../service/api.js';
+document.addEventListener("DOMContentLoaded", function () {
+    loadPresidents();
+});
+
+function loadPresidents() {
+    Api.request("/SketchOnline/Backend/create/getPresidents.php", "GET")
+        .then(response => {
+            console.log(response.status);
+            response.status === 'success' ? OnEventsSuccess(response) : OnEventsError(response);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+function OnEventsSuccess(data) {
+    /*
+    data = {
+        presidents: [
+            {
+                id: 'xxxx',
+                name: 'xxxx'
+            },
+            ...
+        ]
+    };
+    */
+    var list = document.getElementById("president");
+
+    // Loop through the presidents
+    for (var i = 0; i < data.presidents.length; i++) {
+        // Add the president to the list
+        var option = document.createElement("option");
+        option.value = data.presidents[i].id;
+        option.text = data.presidents[i].name;
+        list.appendChild(option);
+    }
+}
+
+function OnEventsError(error) {
+    console.log(error);
+}
 
 function OnSubmit()
 {
     var name = document.getElementById("name").value;
-    var image = document.getElementById("image").value;
+    // var image = document.getElementById("image").value;
+    var president = document.getElementById("president").value;
     var startDate = document.getElementById("startDate").value;
     var endDate = document.getElementById("endDate").value;
     var theme = document.getElementById("theme").value;
@@ -11,17 +53,18 @@ function OnSubmit()
     var rules = document.getElementById("rules").value;
     var criteria = document.getElementById("criteria").value;
 
-    var contest = new FormData();
-    contest.append("name", name);
-    contest.append("image", image);
-    contest.append("startDate", startDate);
-    contest.append("endDate", endDate);
-    contest.append("theme", theme);
-    contest.append("description", description);
-    contest.append("rules", rules);
-    contest.append("criteria", criteria);
-
-    Api.request(/SketchOnline/Backend/contest.php, 'POST', contest)
+    var data = {
+        name: name,
+        president: president,
+        // image: image,
+        startDate: startDate,
+        endDate: endDate,
+        theme: theme,
+        description: description,
+        rules: rules,
+        criteria: criteria
+    }
+    ApiFormData.request("/SketchOnline/Backend/create/createConcours.php", "POST", data)
         .then(response => {
             console.log(response.status);
             response.status === 'success' ? OnSubmitSuccess(response) : OnSubmitError(response);
