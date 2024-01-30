d320047913ea5a14531c55511fc713489f7c5082
Guillaume ROCHE <groche1217@protonmail.com>
2024-01-30 10:32:08 +0100

    patch bug requete

--- Backend/competitions.php
+++ Backend/competitions.php
@@ -1,5 +1,4 @@
 <?php
-echo "<script>console.log('Debug Objects: " . "TEST" . "' );</script>";
 
 require_once('./configdb.php');
 $connexion = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT);
@@ -13,7 +12,6 @@ if ($connexion->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = json_decode(file_get_contents("php://input"));
     $userId = $data->userId;
-    echo "<script>console.log('Debug Objects: " . $userId . "' );</script>";
 
     if ($userId == null) {
         echo json_encode(["status" => "failure", "message" => "userId non fourni"]);
@@ -21,24 +19,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Exécuter la requête
-    $requete = $connexion->prepare("SELECT c.numConcours, c.titre FROM Utilisateurs u JOIN Compétiteur comp ON u.numUtilisateur = comp.numCompetiteur JOIN ParticipeComp pc ON comp.numCompetiteur = pc.numCompetiteur JOIN Concours c ON pc.numConcours = c.numConcours WHERE u.numUtilsateur = ?");
+    $requete = $connexion->prepare("SELECT c.numConcours, c.titre FROM Utilisateurs u JOIN Compétiteur comp ON u.numUtilisateur = comp.numCompetiteur JOIN ParticipeComp pc ON comp.numCompetiteur = pc.numCompetiteur JOIN Concours c ON pc.numConcours = c.numConcours WHERE u.numUtilisateur = ?");
     $requete->bind_param("s", $userId);
 
     $requete->execute();
 
     $result = $requete->get_result();
     $competitionList = [];
-    echo "<script>console.log('Debug Objects: " . $result . "' );</script>";
 
     while ($row = $result->fetch_assoc()) {
-        echo "<script>console.log('Debug Objects: " . $row . "' );</script>";
-
         $competitionList[] = [
             "numConcours" => $row['numConcours'],
             "titre" => $row['titre']
         ];
-
-        echo "<script>console.log('Debug Objects: " . $competitionList . "' );</script>";
     }
 
     $data = [
