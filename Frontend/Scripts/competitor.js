a16c864a2ff7a6b1a4dbd2fd2e7a153eeee1c392
Guillaume ROCHE <groche1217@protonmail.com>
2024-01-30 10:36:35 +0100

    patch bug json

--- Frontend/Scripts/competitor.js
+++ Frontend/Scripts/competitor.js
@@ -9,12 +9,11 @@ function loadContest() {
     var data = localStorage.getItem('userData');    
     var id = JSON.parse(data).userId;
 
-    /*
     var postData = {
         userId: id
     };
 
-    Api.request("/SketchOnline/Backend/competitions.php", "POST", postData)
+    Api.request("/SketchOnline/Backend/competitions.php", "POST", JSON.stringify(postData))
         .then(response => {
             console.log(response.status);
             response.status === 'success' ? OnEventsSuccess(response) : OnEventsError(response);
@@ -22,8 +21,8 @@ function loadContest() {
         .catch((error) => {
             console.error(error);
         });
-    */
 
+    /*
     fetch('/SketchOnline/Backend/competitions.php', {
         method: 'POST',
         headers: {
@@ -38,6 +37,7 @@ function loadContest() {
         .catch((error) => {
             console.error('Error:', error);
         });
+    */
 }
 
 function OnEventsSuccess(data) {
