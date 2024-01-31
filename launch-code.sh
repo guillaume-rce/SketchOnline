ded4422442287256a1a9f88fceb34c87b4be53bc
snoumi <steve.noumi@alfa-safety.fr>
2024-01-31 19:53:59 +0100

    patch launch

--- launch-code.sh
+++ launch-code.sh
@@ -1,7 +1,7 @@
 cd ../
-scp -i vm-SteveNoumi_key.pem -r SketchOnline/ stevenoumi@20.39.244.13:/home/stevenoumi/
+scp -i SketchOnline/vm-SteveNoumi_key.pem -r SketchOnline/ stevenoumi@20.39.244.13:/home/stevenoumi/
 
-ssh -i vm-SteveNoumi_key.pem stevenoumi@20.39.244.13 << EOF
+ssh -i SketchOnline/vm-SteveNoumi_key.pem stevenoumi@20.39.244.13 << EOF
     sudo -i
     rm -rf /srv/siteweb/SketchOnline
     cp -r /home/stevenoumi/SketchOnline/ /srv/siteweb/
